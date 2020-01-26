a0ec1383799b31eedbe0ada5eba7f3922512405f
Jose Orozco <jorozco4258@floridapoly.edu>
2020-01-25 22:03:18 -0500

    Removed main menu, added splash screen (REPLACE WITH ACTUAL ICON SOON)

--- app/src/main/java/com/example/dashcamx/SplashScreen.java
+++ app/src/main/java/com/example/dashcamx/SplashScreen.java
@@ -1,13 +1,18 @@
 package com.example.dashcamx;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
